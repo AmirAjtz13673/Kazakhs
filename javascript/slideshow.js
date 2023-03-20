d01c58f27034d3b254a16e6927f6768f3aca3414
AmirAjtz13673 <127853667+AmirAjtz13673@users.noreply.github.com>
2023-03-19 21:54:23 -0400

    Add files via upload

---
+++ javascript/slideshow.js
@@ -0,0 +1,79 @@
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slide');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+function showSlides() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  setTimeout(showSlides, 5000);
+}
+
+showSlides();
+
+prev.addEventListener('click', function() {
+  slideIndex--;
+  if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slides[slideIndex - 1].style.display = 'block';
+});
+
+next.addEventListener('click', function() {
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  slides[slideIndex - 1].style.display = 'block';
+});
+
+
+
+
+
+  document.addEventListener('keydown', function(event) {
+    if (event.code === 'ArrowLeft') {
+      plusSlides(-1);
+    } else if (event.code === 'ArrowRight') {
+      plusSlides(1);
+    }
+  });
+
+  var slideIndex = 1;
+  showSlides(slideIndex);
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+    }
+    slides[slideIndex-1].classList.add("active");
+    dots[slideIndex-1].classList.add("active");
+  }
